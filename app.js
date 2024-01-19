c98be20a7f9ff6804881e51fc478576897ae0e76
PANCHOv96 <vallonefrancisco@gmail.com>
2024-01-19 15:55:27 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express, { json } from 'express'
+import { peoplesRoutes } from './routes/peoples.js';
+import { filmsRoutes } from './routes/films.js';
+import { planetsRoutes } from './routes/planets.js';
+import { speciesRoutes } from './routes/species.js';
+import { starshipsRoutes } from './routes/starships.js';
+
+const server = express();
+server.use(json())
+
+server.use('/peoples',peoplesRoutes)
+server.use('/films',filmsRoutes)
+server.use('/planets',planetsRoutes)
+server.use('/species',speciesRoutes)
+server.use('/starships',starshipsRoutes)
+
+const PORT = process.env.PORT ?? 1234
+server.listen(PORT,()=>{
+    console.log('SERVER FUNCIONANDO EN PUERTO: ',PORT)
+})
