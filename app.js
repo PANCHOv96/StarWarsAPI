6cedeef09243c74b02f12cb931c3b64d21809b1c
PANCHOv96 <vallonefrancisco@gmail.com>
2024-01-20 14:05:21 -0300

    se agregaron los cors para poder llamar a la API

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ import { filmsRoutes } from './routes/films.js';
 import { planetsRoutes } from './routes/planets.js';
 import { speciesRoutes } from './routes/species.js';
 import { starshipsRoutes } from './routes/starships.js';
+import cors from 'cors'
 
 const server = express();
 server.use(json())
 
+server.use(cors())
 server.use('/peoples',peoplesRoutes)
 server.use('/films',filmsRoutes)
 server.use('/planets',planetsRoutes)
