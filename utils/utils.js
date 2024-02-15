0c975f9c45128278e3c4ff75d4ff4ef5d09d4175
PANCHOv96 <vallonefrancisco@gmail.com>
2024-02-15 13:55:33 -0300

    se elimino la carpeta utils dentro del la carpeta main y se introdujo en el modelo local para un mejor control

--- utils/utils.js
+++
@@ -1,6 +0,0 @@
-//para validar string en local
-// el primer parametro se compara con el segundo pero si este no esta diponible automaticamente retorna TRUE
-export function includesString(name,nameCompare){
-    if(!nameCompare) return true
-    return name.toLowerCase().includes(nameCompare.toLowerCase())
-}
