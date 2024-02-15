18962a6b764fc3eedc9ade62324066b9892c6411
PANCHOv96 <vallonefrancisco@gmail.com>
2024-02-15 13:52:28 -0300

    se creo una funcion para inyeccion de dependencia

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 import express, { json } from 'express'
 import { mainRoutes } from './routes/main.js';
-import { peoplesRoutes } from './routes/peoples.js';
-import { filmsRoutes } from './routes/films.js';
-import { planetsRoutes } from './routes/planets.js';
-import { speciesRoutes } from './routes/species.js';
-import { vehiclesRoutes } from './routes/vehicles.js';
-import { starshipsRoutes } from './routes/starships.js';
+import { createPeoplesRoutes } from './routes/peoples.js';
+import { createFilmsRoutes } from './routes/films.js';
+import { createPlanetsRoutes } from './routes/planets.js';
+import { createSpeciesRoutes } from './routes/species.js';
+import { createStarshipRoutes } from './routes/starships.js';
+import { createVehiclesRoutes } from './routes/vehicles.js';
 import { errorRoutes } from './routes/error.js';
 import cors from 'cors'
 
-const server = express();
-server.use(json())
-
-server.use(cors())
-server.get('/',mainRoutes)
-server.use('/peoples',peoplesRoutes)
-server.use('/films',filmsRoutes)
-server.use('/planets',planetsRoutes)
-server.use('/species',speciesRoutes)
-server.use('/vehicles',vehiclesRoutes)
-server.use('/starships',starshipsRoutes)
-server.get('/*',errorRoutes)
-
-const PORT = process.env.PORT ?? 1234
-server.listen(PORT,()=>{
-    console.log('SERVER FUNCIONANDO EN PUERTO: ',PORT)
-})
+export function createAPP({models,string}){
+    const server = express();
+    server.use(json())
+    
+    server.use(cors())
+    server.get('/',mainRoutes)
+    server.use('/films',createFilmsRoutes({ FilmModel: models.FilmModel }))
+    server.use('/peoples',createPeoplesRoutes({ PeopleModel: models.PeopleModel }))
+    server.use('/planets',createPlanetsRoutes({ PlanetModel: models.PlanetModel }))
+    server.use('/species',createSpeciesRoutes({ SpecieModel: models.SpecieModel }))
+    server.use('/starships',createStarshipRoutes({ StarshipModel: models.StarshipModel }))
+    server.use('/vehicles',createVehiclesRoutes({ VehicleModel: models.VehicleModel }))
+    server.get('/*',errorRoutes)
+    
+    const PORT = process.env.PORT ?? 1234
+    server.listen(PORT,()=>{
+        console.log(string)
+    })
+}
